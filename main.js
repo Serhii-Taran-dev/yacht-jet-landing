adfabc1a22c4cf90f1825401649d60493c91920b
Sergii Taran <serg.taran1970@gmail.com>
2026-08-29 07:58:04 +0300

    add mobile menu interactions

---
+++ main.js
@@ -0,0 +1,35 @@
+const openMenuBtn = document.querySelector(".menu-open-btn");
+const closeMenuBtn = document.querySelector(".mobile-menu-close-btn");
+const mobileMenu = document.querySelector(".mobile-menu");
+const mobileNavLinks = document.querySelectorAll(".mobile-nav-link");
+
+function openMenu() {
+  mobileMenu.classList.add("is-open");
+
+  openMenuBtn.setAttribute("aria-expanded", "true");
+  mobileMenu.setAttribute("aria-hidden", "false");
+
+  document.body.style.overflow = "hidden";
+}
+
+function closeMenu() {
+  mobileMenu.classList.remove("is-open");
+
+  openMenuBtn.setAttribute("aria-expanded", "false");
+  mobileMenu.setAttribute("aria-hidden", "true");
+
+  document.body.style.overflow = "";
+}
+
+openMenuBtn.addEventListener("click", openMenu);
+closeMenuBtn.addEventListener("click", closeMenu);
+
+mobileNavLinks.forEach((link) => {
+  link.addEventListener("click", closeMenu);
+});
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === "Escape" && mobileMenu.classList.contains("is-open")) {
+    closeMenu();
+  }
+});
